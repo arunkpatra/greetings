69af9febd6e6339ffa1266e390dff3def31accb8
Arun K Patra <arunkpatra@gmail.com>
2021-01-16 19:29:58 +0530

    Usage of a slice

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person. An error is returned if an empty name is provided to this
@@ -13,6 +15,24 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns a random format from a fixed set of message formats.
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format
+	return formats[rand.Intn(len(formats))]
 }
